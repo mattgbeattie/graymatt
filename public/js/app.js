3be415538278ff460040671f839d47f20853a622
mattgbeattie <mgb4d@virginia.edu>
2015-06-22 23:28:48 -0400

    added debug console.log() statements

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,3 @@
 angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'TransactionsCtrl', 'TransactionsService', 'AllTransactionsCtrl', 'AllTransactionsService']);
+
+console.log('app ran');
